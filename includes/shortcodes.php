14a2819addc4c6a3ee1e3fd673faeacfdda511ae
dtw <dtw@users.noreply.github.com>
2019-05-18 20:26:03 +0100

    Whitespace

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /*	1. SHORTCODES for Fontawesome ICONS
 
@@ -68,7 +68,7 @@ function scaffold_shortcode_facebook_icon() {
 		return $facebook_icon;
 		}
 	add_shortcode( 'facebook', 'scaffold_shortcode_facebook_icon' );
-	
+
 /* TWITTER icon
 ------------------------ */
 
@@ -76,8 +76,8 @@ function scaffold_shortcode_twitter_icon() {
 	$twitter_icon = "<i class='fab fa-twitter fa-lg shortcode-icon'></i> ";
 		return $twitter_icon;
 		}
-	add_shortcode( 'twitter', 'scaffold_shortcode_twitter_icon' );	
-	
+	add_shortcode( 'twitter', 'scaffold_shortcode_twitter_icon' );
+
 /* SKYPE icon
 ------------------------ */
 
@@ -85,7 +85,7 @@ function scaffold_shortcode_skype_icon() {
 	$skype_icon = "<i class='fab fa-skype fa-lg shortcode-icon'></i> ";
 		return $skype_icon;
 		}
-	add_shortcode( 'skype', 'scaffold_shortcode_skype_icon' );		
+	add_shortcode( 'skype', 'scaffold_shortcode_skype_icon' );
 
 /* LINKEDIN icon
 ------------------------ */
@@ -94,7 +94,7 @@ function scaffold_shortcode_linkedin_icon() {
 	$linkedin_icon = "<i class='fab fa-linkedin fa-lg shortcode-icon'></i> ";
 		return $linkedin_icon;
 		}
-	add_shortcode( 'linkedin', 'scaffold_shortcode_linkedin_icon' );		
+	add_shortcode( 'linkedin', 'scaffold_shortcode_linkedin_icon' );
 
 /* YOUTUBE icon
 ------------------------ */
@@ -103,7 +103,7 @@ function scaffold_shortcode_youtube_icon() {
 	$youtube_icon = "<i class='fab fa-youtube fa-lg shortcode-icon'></i> ";
 		return $youtube_icon;
 		}
-	add_shortcode( 'youtube', 'scaffold_shortcode_youtube_icon' );		
+	add_shortcode( 'youtube', 'scaffold_shortcode_youtube_icon' );
 
 /* CALENDAR icon
 ------------------------ */
@@ -112,7 +112,7 @@ function scaffold_shortcode_calendar_icon() {
 	$calendar_icon = "<i class='fa fa-calendar fa-lg shortcode-icon'></i> ";
 		return $calendar_icon;
 		}
-	add_shortcode( 'calendar', 'scaffold_shortcode_calendar_icon' );		
+	add_shortcode( 'calendar', 'scaffold_shortcode_calendar_icon' );
 
 /* STAR icon
 ------------------------ */
@@ -121,7 +121,7 @@ function scaffold_shortcode_star_icon() {
 	$star_icon = "<i class='fa fa-star fa-lg shortcode-icon'></i> ";
 		return $star_icon;
 		}
-	add_shortcode( 'star', 'scaffold_shortcode_star_icon' );		
+	add_shortcode( 'star', 'scaffold_shortcode_star_icon' );
 
 /* GREEN STAR icon
 ------------------------ */
@@ -130,7 +130,7 @@ function scaffold_shortcode_greenstar_icon() {
 	$greenstar_icon = "<i class='fa fa-star fa-lg shortcode-icon' style='color:#84BF41;'></i> ";
 		return $greenstar_icon;
 		}
-	add_shortcode( 'greenstar', 'scaffold_shortcode_greenstar_icon' );		
+	add_shortcode( 'greenstar', 'scaffold_shortcode_greenstar_icon' );
 
 /* LOCATION icon
 ------------------------ */
@@ -139,7 +139,7 @@ function scaffold_shortcode_location_icon() {
 	$location_icon = "<i class='fas fa-map-marker-alt fas-lg shortcode-icon'></i> ";
 		return $location_icon;
 		}
-	add_shortcode( 'location', 'scaffold_shortcode_location_icon' );		
+	add_shortcode( 'location', 'scaffold_shortcode_location_icon' );
 
 /* WARNING icon
 ------------------------ */
@@ -148,7 +148,7 @@ function scaffold_shortcode_warning_icon() {
 	$warning_icon = "<i class='fa fa-warning fa-lg shortcode-icon'></i> ";
 		return $warning_icon;
 		}
-	add_shortcode( 'warning', 'scaffold_shortcode_warning_icon' );		
+	add_shortcode( 'warning', 'scaffold_shortcode_warning_icon' );
 
 /* ADDRESS icon
 ------------------------ */
@@ -157,8 +157,8 @@ function scaffold_shortcode_address_icon() {
 	$address_icon = "<i class='fa fa-pencil-alt fa-lg shortcode-icon'></i> ";
 		return $address_icon;
 		}
-	add_shortcode( 'address', 'scaffold_shortcode_address_icon' );	
-	
+	add_shortcode( 'address', 'scaffold_shortcode_address_icon' );
+
 /* WEBSITE icon
 ------------------------ */
 
@@ -166,11 +166,11 @@ function scaffold_shortcode_website_icon() {
 	$website_icon = "<i class='fa fa-link fa-lg shortcode-icon'></i> ";
 		return $website_icon;
 		}
-	add_shortcode( 'website', 'scaffold_shortcode_website_icon' );	
-	
-		
-	
-	
+	add_shortcode( 'website', 'scaffold_shortcode_website_icon' );
+
+
+
+
 /* 2. BOOTSTRAP grid
 ------------------------ */
 
@@ -178,30 +178,30 @@ function scaffold_shortcode_bootstrap_row_start() {
 	$bootstrap_row_start = "<div class='row'>";
 		return $bootstrap_row_start;
 		}
-	add_shortcode( 'row', 'scaffold_shortcode_bootstrap_row_start' );	
+	add_shortcode( 'row', 'scaffold_shortcode_bootstrap_row_start' );
 
 function scaffold_shortcode_bootstrap_row_end() {
 	$bootstrap_row_end = "</div><!-- end of row -->";
 		return $bootstrap_row_end;
 		}
-	add_shortcode( 'endrow', 'scaffold_shortcode_bootstrap_row_end' );	
-	
+	add_shortcode( 'endrow', 'scaffold_shortcode_bootstrap_row_end' );
+
 function scaffold_shortcode_bootstrap_column_start() {
 	$bootstrap_col_start = "<div class='col-md-6'>";
 		return $bootstrap_col_start;
 		}
-	add_shortcode( 'col', 'scaffold_shortcode_bootstrap_column_start' );	
-		
+	add_shortcode( 'col', 'scaffold_shortcode_bootstrap_column_start' );
+
 function scaffold_shortcode_bootstrap_column_end() {
 	$bootstrap_col_end = "</div><!-- end of column -->";
 		return $bootstrap_col_end;
 		}
-	add_shortcode( 'endcol', 'scaffold_shortcode_bootstrap_column_end' );	
+	add_shortcode( 'endcol', 'scaffold_shortcode_bootstrap_column_end' );
 
 
 
-	
-/* 2. Other SHORTCODES
+
+/* 3. Other SHORTCODES
 ------------------------ */
 
 add_shortcode('search', 'get_search_form');
