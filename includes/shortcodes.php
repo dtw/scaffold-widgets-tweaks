2ea322ddd979b0049334b251381c4b52e1555d5d
dtw <dtw@users.noreply.github.com>
2019-05-30 10:27:51 +0100

    Add shortcodes with revised names

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -27,149 +27,300 @@
 /* PHONE icon
 ------------------------ */
 
-function scaffold_shortcode_phone_icon() {
+function old_scaffold_shortcode_phone_icon() {
 	$phone_icon = "<i class='fa fa-phone fa-lg shortcode-icon'></i> ";
 		return $phone_icon;
 		}
-	add_shortcode( 'phone', 'scaffold_shortcode_phone_icon' );
+	add_shortcode( 'phone', 'old_scaffold_shortcode_phone_icon' );
 
 /* TELEPHONE icon
 ------------------------ */
 
-function scaffold_shortcode_telephone_icon() {
+function old_scaffold_shortcode_telephone_icon() {
 	$telephone_icon = "<i class='fa fa-phone fa-lg shortcode-icon'></i> ";
 		return $telephone_icon;
 		}
-	add_shortcode( 'telephone', 'scaffold_shortcode_telephone_icon' );
+	add_shortcode( 'telephone', 'old_scaffold_shortcode_telephone_icon' );
 
 /* EMAIL icon
 ------------------------ */
 
-function scaffold_shortcode_email_icon() {
+function old_scaffold_shortcode_email_icon() {
 	$email_icon = "<i class='far fa-envelope fa-lg shortcode-icon'></i> ";
 		return $email_icon;
 		}
-	add_shortcode( 'email', 'scaffold_shortcode_email_icon' );
+	add_shortcode( 'email', 'old_scaffold_shortcode_email_icon' );
 
 /* INFO icon
 ------------------------ */
 
-function scaffold_shortcode_info_icon() {
+function old_scaffold_shortcode_info_icon() {
 	$info_icon = "<i class='fa fa-info-circle fa-lg shortcode-icon'></i> ";
 		return $info_icon;
 		}
-	add_shortcode( 'info', 'scaffold_shortcode_info_icon' );
+	add_shortcode( 'info', 'old_scaffold_shortcode_info_icon' );
 
 /* FACEBOOK icon
 ------------------------ */
 
-function scaffold_shortcode_facebook_icon() {
+function old_scaffold_shortcode_facebook_icon() {
 	$facebook_icon = "<i class='fab fa-facebook fa-lg shortcode-icon'></i> ";
 		return $facebook_icon;
 		}
-	add_shortcode( 'facebook', 'scaffold_shortcode_facebook_icon' );
+	add_shortcode( 'facebook', 'old_scaffold_shortcode_facebook_icon' );
 
 /* TWITTER icon
 ------------------------ */
 
-function scaffold_shortcode_twitter_icon() {
+function old_scaffold_shortcode_twitter_icon() {
 	$twitter_icon = "<i class='fab fa-twitter fa-lg shortcode-icon'></i> ";
 		return $twitter_icon;
 		}
-	add_shortcode( 'twitter', 'scaffold_shortcode_twitter_icon' );
+	add_shortcode( 'twitter', 'old_scaffold_shortcode_twitter_icon' );
 
 /* SKYPE icon
 ------------------------ */
 
-function scaffold_shortcode_skype_icon() {
+function old_scaffold_shortcode_skype_icon() {
 	$skype_icon = "<i class='fab fa-skype fa-lg shortcode-icon'></i> ";
 		return $skype_icon;
 		}
-	add_shortcode( 'skype', 'scaffold_shortcode_skype_icon' );
+	add_shortcode( 'skype', 'old_scaffold_shortcode_skype_icon' );
 
 /* LINKEDIN icon
 ------------------------ */
 
-function scaffold_shortcode_linkedin_icon() {
+function old_scaffold_shortcode_linkedin_icon() {
 	$linkedin_icon = "<i class='fab fa-linkedin fa-lg shortcode-icon'></i> ";
 		return $linkedin_icon;
 		}
-	add_shortcode( 'linkedin', 'scaffold_shortcode_linkedin_icon' );
+	add_shortcode( 'linkedin', 'old_scaffold_shortcode_linkedin_icon' );
 
 /* YOUTUBE icon
 ------------------------ */
 
-function scaffold_shortcode_youtube_icon() {
+function old_scaffold_shortcode_youtube_icon() {
 	$youtube_icon = "<i class='fab fa-youtube fa-lg shortcode-icon'></i> ";
 		return $youtube_icon;
 		}
-	add_shortcode( 'youtube', 'scaffold_shortcode_youtube_icon' );
+	add_shortcode( 'youtube', 'old_scaffold_shortcode_youtube_icon' );
 
 /* CALENDAR icon
 ------------------------ */
 
-function scaffold_shortcode_calendar_icon() {
+function old_scaffold_shortcode_calendar_icon() {
 	$calendar_icon = "<i class='fa fa-calendar fa-lg shortcode-icon'></i> ";
 		return $calendar_icon;
 		}
-	add_shortcode( 'calendar', 'scaffold_shortcode_calendar_icon' );
+	add_shortcode( 'calendar', 'old_scaffold_shortcode_calendar_icon' );
 
 /* STAR icon
 ------------------------ */
 
-function scaffold_shortcode_star_icon() {
+function old_scaffold_shortcode_star_icon() {
 	$star_icon = "<i class='fa fa-star fa-lg shortcode-icon'></i> ";
 		return $star_icon;
 		}
-	add_shortcode( 'star', 'scaffold_shortcode_star_icon' );
+	add_shortcode( 'star', 'old_scaffold_shortcode_star_icon' );
 
 /* GREEN STAR icon
 ------------------------ */
 
-function scaffold_shortcode_greenstar_icon() {
+function old_scaffold_shortcode_greenstar_icon() {
 	$greenstar_icon = "<i class='fa fa-star fa-lg shortcode-icon' style='color:#84BF41;'></i> ";
 		return $greenstar_icon;
 		}
-	add_shortcode( 'greenstar', 'scaffold_shortcode_greenstar_icon' );
+	add_shortcode( 'greenstar', 'old_scaffold_shortcode_greenstar_icon' );
 
 /* LOCATION icon
 ------------------------ */
 
-function scaffold_shortcode_location_icon() {
+function old_scaffold_shortcode_location_icon() {
 	$location_icon = "<i class='fas fa-map-marker-alt fas-lg shortcode-icon'></i> ";
 		return $location_icon;
 		}
-	add_shortcode( 'location', 'scaffold_shortcode_location_icon' );
+	add_shortcode( 'location', 'old_scaffold_shortcode_location_icon' );
 
 /* WARNING icon
 ------------------------ */
 
-function scaffold_shortcode_warning_icon() {
+function old_scaffold_shortcode_warning_icon() {
 	$warning_icon = "<i class='fa fa-warning fa-lg shortcode-icon'></i> ";
 		return $warning_icon;
 		}
-	add_shortcode( 'warning', 'scaffold_shortcode_warning_icon' );
+	add_shortcode( 'warning', 'old_scaffold_shortcode_warning_icon' );
 
 /* ADDRESS icon
 ------------------------ */
 
-function scaffold_shortcode_address_icon() {
+function old_scaffold_shortcode_address_icon() {
 	$address_icon = "<i class='fa fa-pencil-alt fa-lg shortcode-icon'></i> ";
 		return $address_icon;
 		}
-	add_shortcode( 'address', 'scaffold_shortcode_address_icon' );
+	add_shortcode( 'address', 'old_scaffold_shortcode_address_icon' );
 
 /* WEBSITE icon
 ------------------------ */
 
-function scaffold_shortcode_website_icon() {
+function old_scaffold_shortcode_website_icon() {
 	$website_icon = "<i class='fas fa-external-link-alt fa-lg shortcode-icon'></i> ";
 		return $website_icon;
 		}
-	add_shortcode( 'website', 'scaffold_shortcode_website_icon' );
+	add_shortcode( 'website', 'old_scaffold_shortcode_website_icon' );
+
+	/*
 
 
 
+	NEW VERSIONS!!!!!!!!!!!!!!
+
+
+
+	*/
+
+	/* PHONE icon
+	------------------------ */
+
+	function scaffold_shortcode_phone_icon() {
+		$phone_icon = "<i class='fa fa-phone fa-lg shortcode-icon'></i> ";
+			return $phone_icon;
+			}
+		add_shortcode( 'icon_phone', 'scaffold_shortcode_phone_icon' );
+
+	/* TELEPHONE icon
+	------------------------ */
+
+	function scaffold_shortcode_telephone_icon() {
+		$telephone_icon = "<i class='fa fa-phone fa-lg shortcode-icon'></i> ";
+			return $telephone_icon;
+			}
+		add_shortcode( 'icon_telephone', 'scaffold_shortcode_telephone_icon' );
+
+	/* EMAIL icon
+	------------------------ */
+
+	function scaffold_shortcode_email_icon() {
+		$email_icon = "<i class='far fa-envelope fa-lg shortcode-icon'></i> ";
+			return $email_icon;
+			}
+		add_shortcode( 'icon_email', 'scaffold_shortcode_email_icon' );
+
+	/* INFO icon
+	------------------------ */
+
+	function scaffold_shortcode_info_icon() {
+		$info_icon = "<i class='fa fa-info-circle fa-lg shortcode-icon'></i> ";
+			return $info_icon;
+			}
+		add_shortcode( 'icon_info', 'scaffold_shortcode_info_icon' );
+
+	/* FACEBOOK icon
+	------------------------ */
+
+	function scaffold_shortcode_facebook_icon() {
+		$facebook_icon = "<i class='fab fa-facebook fa-lg shortcode-icon'></i> ";
+			return $facebook_icon;
+			}
+		add_shortcode( 'icon_facebook', 'scaffold_shortcode_facebook_icon' );
+
+	/* TWITTER icon
+	------------------------ */
+
+	function scaffold_shortcode_twitter_icon() {
+		$twitter_icon = "<i class='fab fa-twitter fa-lg shortcode-icon'></i> ";
+			return $twitter_icon;
+			}
+		add_shortcode( 'icon_twitter', 'scaffold_shortcode_twitter_icon' );
+
+	/* SKYPE icon
+	------------------------ */
+
+	function scaffold_shortcode_skype_icon() {
+		$skype_icon = "<i class='fab fa-skype fa-lg shortcode-icon'></i> ";
+			return $skype_icon;
+			}
+		add_shortcode( 'icon_skype', 'scaffold_shortcode_skype_icon' );
+
+	/* LINKEDIN icon
+	------------------------ */
+
+	function scaffold_shortcode_linkedin_icon() {
+		$linkedin_icon = "<i class='fab fa-linkedin fa-lg shortcode-icon'></i> ";
+			return $linkedin_icon;
+			}
+		add_shortcode( 'icon_linkedin', 'scaffold_shortcode_linkedin_icon' );
+
+	/* YOUTUBE icon
+	------------------------ */
+
+	function scaffold_shortcode_youtube_icon() {
+		$youtube_icon = "<i class='fab fa-youtube fa-lg shortcode-icon'></i> ";
+			return $youtube_icon;
+			}
+		add_shortcode( 'icon_youtube', 'scaffold_shortcode_youtube_icon' );
+
+	/* CALENDAR icon
+	------------------------ */
+
+	function scaffold_shortcode_calendar_icon() {
+		$calendar_icon = "<i class='fa fa-calendar fa-lg shortcode-icon'></i> ";
+			return $calendar_icon;
+			}
+		add_shortcode( 'icon_calendar', 'scaffold_shortcode_calendar_icon' );
+
+	/* STAR icon
+	------------------------ */
+
+	function scaffold_shortcode_star_icon() {
+		$star_icon = "<i class='fa fa-star fa-lg shortcode-icon'></i> ";
+			return $star_icon;
+			}
+		add_shortcode( 'icon_star', 'scaffold_shortcode_star_icon' );
+
+	/* GREEN STAR icon
+	------------------------ */
+
+	function scaffold_shortcode_greenstar_icon() {
+		$greenstar_icon = "<i class='fa fa-star fa-lg shortcode-icon' style='color:#84BF41;'></i> ";
+			return $greenstar_icon;
+			}
+		add_shortcode( 'icon_greenstar', 'scaffold_shortcode_greenstar_icon' );
+
+	/* LOCATION icon
+	------------------------ */
+
+	function scaffold_shortcode_location_icon() {
+		$location_icon = "<i class='fas fa-map-marker-alt fas-lg shortcode-icon'></i> ";
+			return $location_icon;
+			}
+		add_shortcode( 'icon_location', 'scaffold_shortcode_location_icon' );
+
+	/* WARNING icon
+	------------------------ */
+
+	function scaffold_shortcode_warning_icon() {
+		$warning_icon = "<i class='fa fa-warning fa-lg shortcode-icon'></i> ";
+			return $warning_icon;
+			}
+		add_shortcode( 'icon_warning', 'scaffold_shortcode_warning_icon' );
+
+	/* ADDRESS icon
+	------------------------ */
+
+	function scaffold_shortcode_address_icon() {
+		$address_icon = "<i class='fa fa-pencil-alt fa-lg shortcode-icon'></i> ";
+			return $address_icon;
+			}
+		add_shortcode( 'icon_address', 'scaffold_shortcode_address_icon' );
+
+	/* WEBSITE icon
+	------------------------ */
+
+	function scaffold_shortcode_website_icon() {
+		$website_icon = "<i class='fas fa-external-link-alt fa-lg shortcode-icon'></i> ";
+			return $website_icon;
+			}
+		add_shortcode( 'icon_website', 'scaffold_shortcode_website_icon' );
 
 /* 2. BOOTSTRAP grid
 ------------------------ */
