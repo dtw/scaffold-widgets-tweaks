160981df76526abc418671fa0c0656e9b41d4266
dtw <dtw@users.noreply.github.com>
2019-06-14 11:16:21 +0100

    Reduce delay

--- js/accordion_scroll.js
+++ js/accordion_scroll.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   $('.panel-collapse').on('shown.bs.collapse', function(event) {
     var $panel = $(this).closest('.panel');
     $('html,body').animate({
-    }, 500);
       scrollTop: ($panel.offset().top - $('.navbar').outerHeight())
+    }, 250);
   });
 });
 
