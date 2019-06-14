e441487b6045b3ca5e389bee482032deb66cbcc2
dtw <dtw@users.noreply.github.com>
2019-06-14 11:08:47 +0100

    Fix dogs dinner

--- js/accordion_scroll.js
+++ js/accordion_scroll.js
@@ -1,8 +1,10 @@
 // automatically scrolls to the top of the active accordion panel
-  jQuery('.panel-collapse').on('shown.bs.collapse', function(event) {
+$(document).ready(function(){
+  $('.panel-collapse').on('shown.bs.collapse', function(event) {
     var $panel = $(this).closest('.panel');
-    jQuery('html,body').animate({
-      scrollTop: $panel.offset().top
+    $('html,body').animate({
+      scrollTop: ($panel.offset().top - 76)
     }, 500);
   });
+});
 
