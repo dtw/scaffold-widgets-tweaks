0e24ccfae1e41ed90ec6fe4ea519f30d98d986d4
dtw <dtw@users.noreply.github.com>
2019-10-18 22:49:00 +0100

    Update three-column widget

--- includes/widget-three-column-hwbucks.php
+++ includes/widget-three-column-hwbucks.php
@@ -7,27 +7,28 @@ Based on code by Jason King - http://kingjason.com - cheers Jason
 /**
  * Register the widget
  */
-function SF_register_widget_hwbucks_featured_post() {
-	register_widget( 'SF_HWBucks_Featured_Post_Widget' );
+function SF_register_widget_hwbucks_three_column() {
+	register_widget( 'SF_HWBucks_Three_Column_Widget' );
 }
-add_action( 'widgets_init', 'SF_register_widget_hwbucks_featured_post' );
+add_action( 'widgets_init', 'SF_register_widget_hwbucks_three_column' );
 /**
- * A Featured Post Widget
- * Feature a post, showing its excerpt and thumbnail.
+ * A Three Column Widget
+ *
+ * Add three links to posts or pages with header and text
  *
  */
-class SF_HWBucks_Featured_Post_Widget extends WP_Widget {
+class SF_HWBucks_Three_Column_Widget extends WP_Widget {
 	/**
 	 * Sets up a new widget instance.
 	 *
 	 * @access public
 	 */
-	function SF_HWBucks_Featured_Post_Widget() {
-		parent::WP_Widget( 'SF_HWBucks_Featured_Post_Widget',
-		$name = 'HW Featured Post',
+	function SF_HWBucks_Three_Column_Widget() {
+		parent::WP_Widget( 'SF_HWBucks_Three_Column_Widget',
+		$name = 'HW Three Column',
 		array(
-			'classname'   => 'scaffold_widget_hwbucks_featured_post widget_featured_post',
-			'description' => 'Display a specific post title, excerpt and featured image (choose most recent 10 posts)'
+			'classname'   => 'scaffold_widget_hwbucks_three_column widget_three_column',
+			'description' => 'Display links to posts or pages in a three column layout'
 	)
 		);
 	}
@@ -41,130 +42,65 @@ class SF_HWBucks_Featured_Post_Widget extends WP_Widget {
 	 */
 	function widget( $args, $instance ) {
 		extract( $args );
-		if ( isset( $instance['post'] ) && $instance['post'] != -1 ) {
-			$post_id = (int) $instance['post'];
-			$title = $instance['title'] ;
-			$panel_colour = $instance['panel_colour'] ;
-			$btn_text = $instance['btn_text'] ;
-			$p = new WP_Query( array( 'p' => $post_id ) );
 
-			if ( $p->have_posts() ) {
-				$p->the_post();
+			$title_1 = $instance['title'] ;
+			$body_text_1 = $instance['body_text'] ;
+			$url_1 = $instance['url'];
+			$btn_text_1 = $instance['btn_text'] ;
 
 				echo $before_widget;
-				?>
-
-				<?php echo '<div class="col-md-12 col-sm-12 col-xs-12 panel panel-' . $panel_colour . '">'?>
-					<div class="row">
-						<div class="col-md-8 col-sm-6 col-xs-12 panel-text">
-							<div class="row">
-								<div class="col-md-12 panel-title">
-									<h2><?php echo $title ?></h2>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-12 col-sm-12 col-xs-12">
-									<a class="title-link" href="<?php the_permalink(); ?>" rel="bookmark">
-										<?php the_title(); ?>
-									</a>
-									<?php the_excerpt(); ?>
-									<p style="clear: both;"><a class="btn btn-primary" href="<?php echo get_the_permalink();
-										echo '">';
-										echo $btn_text; ?>
-									</a></p>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-6 hidden-xs panel-icon">
-							<a href="
-								<?php the_permalink(); ?>" rel="bookmark">
-								<?php the_post_thumbnail([auto,240]); ?>
-							</a>
-						</div>
+				for ($i = 1; $i <= 3; $i++) { ?>
+					<div class="col-md-4 col-sm-4 col-xs-12 subitem">
+						<h3>
+							<?php echo '<a href="' . $instance['url_'.$i] . '">' . $instance['title_'.$i] . '</a>'?>
+						</h3>
+						<?php echo '<p>' . $instance['body_text_'.$i] . '</p>'?>
+						<p>At Healthwatch Bucks we believe that we all learn from each other. Make sure that what people have to say helps you do better every day; our client services can help.</p>
+						<p>
+							<?php echo '<a href="' . $instance['url_'.$i] . '">' . $instance['btn_text_'.$i] . '&raquo;</a>'?>
+						</p>
 					</div>
-				</div>
-				<?php
+				<?php }
 				echo $after_widget;
-				wp_reset_postdata();
-			}
-		}
 	}
 
 	// Save widget settings
 
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['post'] = (int)( $new_instance['post'] );
-		$instance['title'] = wp_strip_all_tags( $new_instance['title'] );
-		$instance['panel_colour'] = wp_strip_all_tags( $new_instance['panel_colour'] );
-		$instance['btn_text'] = wp_strip_all_tags( $new_instance['btn_text'] );
-		return $instance;
+		for ($i = 1; $i <= 3; $i++) {
+			$instance['title_'.$i] = wp_strip_all_tags( $new_instance['title_'.$i] );
+			$instance['url_'.$i] = wp_strip_all_tags( $new_instance['url_'.$i] );
+			$instance['body_text_'.$i] = wp_strip_all_tags( $new_instance['body_text_'.$i] );
+			$instance['btn_text_'.$i] = wp_strip_all_tags( $new_instance['btn_text_'.$i] );
+			return $instance;
+		}
 	}
 	function form( $instance ) {
-		$post = isset( $instance['post'] ) ? (int) $instance['post'] : -1;
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : 'Hot news';
-		$panel_colour = ! empty( $instance['panel_colour'] ) ? $instance['panel_colour'] : 'orange';
-		$btn_text = ! empty( $instance['btn_text'] ) ? $instance['btn_text'] : 'Read more';
+		for ($i = 1; $i <= 3; $i++) {
+			$title = ! empty( $instance['title_'.$i] ) ? $instance['title_'.$i] : 'Title ' . $i;
+			$url = ! empty( $instance['url_'.$i] ) ? $instance['url_'.$i] : 'URL ' . $i;
+			$body_text = ! empty( $instance['body_text_'.$i] ) ? $instance['body_text_'.$i] : 'Some text.';
+			$btn_text = ! empty( $instance['btn_text_'.$i] ) ? $instance['btn_text_'.$i] : 'Read more';
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Content title:</label>
-			<input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'title_'.$i ); ?>">Content title:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'title_'.$i ); ?>" name="<?php echo $this->get_field_name( 'title_'.$i ); ?>" value="<?php echo esc_attr( $title ); ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('panel_colour'); ?>">Panel colour:
-				<select class='widefat' id="<?php echo $this->get_field_id('panel_colour'); ?>"
-						 name="<?php echo $this->get_field_name('panel_colour'); ?>" type="text">
-					<?php
-					/* This array and loop generates the rows for the dropdown menu. Blue results in panel-blue. Matching styles required in CSS */
-					$colourArray = ["Orange", "Blue", "Green", "Pink", "Turquoise"];
-						foreach ($colourArray as $colour)  {
-							echo "<option value='" . strtolower($colour) . "'";
-							echo ($panel_colour==strtolower($colour))?'selected':'';
-							echo ">" . $colour . "</option>";
-						}
-					?>
-				</select>
-			</label>
+			<label for="<?php echo $this->get_field_id( 'body_text_'.$i ); ?>">Body text:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'body_text_'.$i ); ?>" name="<?php echo $this->get_field_name( 'body_text_'.$i ); ?>" value="<?php echo esc_attr( $body_text ); ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'btn_text' ); ?>">Button text:</label>
-			<input type="text" id="<?php echo $this->get_field_id( 'btn_text' ); ?>" name="<?php echo $this->get_field_name( 'btn_text' ); ?>" value="<?php echo esc_attr( $btn_text ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'url_'.$i ); ?>">URL text:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'url_'.$i ); ?>" name="<?php echo $this->get_field_name( 'url_'.$i ); ?>" value="<?php echo esc_attr( $url ); ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'post' ); ?>">Which post?
-
-			<?php
-			/**
-			 *  Can't see how to walk posts like pages so we'll just get the last 10, which is probably adequate!
-			 */
-			 		$args = array(
-							'post_type' => 'post',
-							'numberposts' => '10',
-							'post_status' => 'publish',
-							//'order' => 'asc' // rather than changing the sort order, this gets the oldest x posts first!
-	        );
-	        $recent_posts = wp_get_recent_posts($args);
-	        if ( ! empty( $recent_posts ) ) { ?>
-						<select class='widefat' id="<?php echo $this->get_field_id('post'); ?>" name="<?php echo $this->get_field_name('post'); ?>">
-            	<option value="-1">Select a post</option>
-            	<?php
-								foreach ($recent_posts as $recent)  {
-									echo "<option value='" . $recent["ID"] . "'";
-									echo ($post==$recent["ID"])?'selected':'';
-									echo ">" . $recent["post_title"] . "</option>";
-								}
-							?>;
-            </select>
-					</label>
-	        <?php
-					} else { ?>
-						<p>No results!</p>
-
-					<?php
-					}
-	        ?>
-        </p>
+			<label for="<?php echo $this->get_field_id( 'btn_text_'.$i ); ?>">URL text:</label>
+			<input type="text" id="<?php echo $this->get_field_id( 'btn_text_'.$i ); ?>" name="<?php echo $this->get_field_name( 'btn_text_'.$i ); ?>" value="<?php echo esc_attr( $btn_text ); ?>" />
+		</p>
 	<?php
+		}
 	}
 }
 ?>
