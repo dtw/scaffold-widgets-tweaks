0025c1e4aa89e1600add372c08790e2c1bdfccba
dtw <dtw@users.noreply.github.com>
2019-10-25 13:44:57 +0100

    Add widefat class

--- includes/widget-three-column-img-hwbucks.php
+++ includes/widget-three-column-img-hwbucks.php
@@ -99,7 +99,7 @@ class SF_HWBucks_Three_Col_Img_Widget extends WP_Widget {
 			</p>
 			<p>
 				<label for="<?php echo $this->get_field_id( 'img_url_'.$i ); ?>">Image URL:</label>
-				<input type="text" id="<?php echo $this->get_field_id( 'img_url_'.$i ); ?>" name="<?php echo $this->get_field_name( 'img_url_'.$i ); ?>" value="<?php echo esc_attr( $img_url ); ?>" />
+				<input class="widefat" type="text" id="<?php echo $this->get_field_id( 'img_url_'.$i ); ?>" name="<?php echo $this->get_field_name( 'img_url_'.$i ); ?>" value="<?php echo esc_attr( $img_url ); ?>" />
 			</p>
 		</div>
 	<?php
