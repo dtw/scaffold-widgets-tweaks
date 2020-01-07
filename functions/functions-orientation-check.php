58a4d8f38810d49bdf29838f4476e2ca58725cd8
dtw <dtw@users.noreply.github.com>
2020-01-07 14:55:24 +0000

    Fix brackets

--- functions/functions-orientation-check.php
+++ functions/functions-orientation-check.php
@@ -2,7 +2,7 @@
 	function orientation_check($image_id) {
     $image_attributes = wp_get_attachment_image_src( $attachment_id = $image_id, 'full' );
     // returns url, width, height, is_intermediate
-    if (abs(($image_attributes[1] - $image_attributes[2])) <= 10) {
+    if ((abs($image_attributes[1] - $image_attributes[2])) <= 10) {
       // it's roughly square
       return 'sq';
     } elseif ($image_attributes[1] > $image_attributes[2]) {
