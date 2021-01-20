cd3c284c9c95846628a2b82acb8a7c5ffee59247
dtw <dtw@users.noreply.github.com>
2021-01-20 15:48:42 +0000

    Update title

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -775,7 +775,7 @@ function scaffold_shortcode_mailchimp_signup() {
 	<!-- Begin MailChimp Signup Form -->
 	<div id="mailchimp-container">
 		<div id="mailchimp-title">
-			<h2>Subscribe to the Healthwatch Bucks email newsletter</h2>
+			<h2>Subscribe to the Healthwatch Bucks newsletter</h2>
 		</div>
 		<div id="mailchimp-form">
 			<form action="//healthwatchbucks.us3.list-manage.com/subscribe/post?u=b3e99f9452fa226631c515b62&amp;id=bc05bac5eb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" style="margin:0;" target="_blank" novalidate>
