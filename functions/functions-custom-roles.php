34170149fd94fee9c0c5ef7f041fa7ad28cd7036
Phil T <362017+dtw@users.noreply.github.com>
2022-06-07 09:20:32 +0100

    Update comments, remove old code

--- functions/functions-custom-roles.php
+++ functions/functions-custom-roles.php
@@ -11,7 +11,6 @@
     $role->add_cap( 'customize' );
     $role->add_cap( 'edit_theme_options' );
   }
-  //add_action( 'init', 'create_editor_plus_role' );
 
   function remove_editor_plus_role() {
     // gets the editor_plus role
@@ -19,10 +18,9 @@
     // del capabilities
     $role->remove_cap( 'customize' );
     $role->remove_cap( 'edit_theme_options' );
-    // remove the new role
+    // remove the editor_plus role
     remove_role( 'editor_plus', 'Editor Plus', get_role( 'editor' )->capabilities );
   }
-  //add_action( 'init', 'remove_editor_plus_role' );
 
   /**
    * Register the "Subscriber Plus" role
@@ -35,14 +33,13 @@
     // add capabilities
     $role->add_cap( 'read_private_posts' );
   }
-  //add_action( 'init', 'create_subscriber_plus_role' );
 
   function remove_subscriber_plus_role() {
     // gets the subscriber_plus role
     $role = get_role( 'subscriber_plus' );
     // del capabilities
     $role->remove_cap( 'read_private_posts' );
-    // remove the new role
+    // remove the subscriber_plus role role
     remove_role( 'subscriber_plus', 'Subscriber Plus', get_role( 'subscriber' )->capabilities );
   }
 ?>
