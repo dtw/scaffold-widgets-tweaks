e8f4a7837060c88295bfa5a55904527c66d5ddbe
Phil T <362017+dtw@users.noreply.github.com>
2022-06-08 07:58:39 +0100

    Whitespace

--- includes/dash.php
+++ includes/dash.php
@@ -93,53 +93,52 @@ add_action( 'after_setup_theme', 'scaffold_add_editor_styles' );
 add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
 
 function my_custom_dashboard_widgets() {
-global $wp_meta_boxes;
-
-wp_add_dashboard_widget('custom_help_widget', 'Get website support', 'custom_dashboard_help');
+  global $wp_meta_boxes;
+  wp_add_dashboard_widget('custom_help_widget', 'Get website support', 'custom_dashboard_help');
 }
 
 function custom_dashboard_help() {
 
-$url = home_url('');
+  $url = home_url('');
 
-$google_mobile_friendly_url = 'https://www.google.co.uk/webmasters/tools/mobile-friendly/?url=' . $url;
+  $google_mobile_friendly_url = 'https://www.google.co.uk/webmasters/tools/mobile-friendly/?url=' . $url;
 
-$twitter_url = 'http://www.twitter.com/jasoncsking';
+  $twitter_url = 'http://www.twitter.com/jasoncsking';
 
-$rtfm = 'https://docs.google.com/document/d/1Vdji4IOJnPP8akjGdVGLP_i6je165qBg_rZbWkr05I8/edit?usp=sharing';
+  $rtfm = 'https://docs.google.com/document/d/1Vdji4IOJnPP8akjGdVGLP_i6je165qBg_rZbWkr05I8/edit?usp=sharing';
 
-$my_theme = wp_get_theme();
-$my_theme_name = $my_theme->get( 'Name' );
-$my_theme_version = $my_theme->get( 'Version' );
+  $my_theme = wp_get_theme();
+  $my_theme_name = $my_theme->get( 'Name' );
+  $my_theme_version = $my_theme->get( 'Version' );
 
-echo '
+  echo '
 
-<p><img src="' . $url . '/wp-content/themes/scaffold/screenshot.png" width="140" style="float:right;" />Your <strong>WordPress</strong> theme is <strong>' . $my_theme_name . ' version ' . $my_theme_version . '</strong>.</p>
+  <p><img src="' . $url . '/wp-content/themes/scaffold/screenshot.png" width="140" style="float:right;" />Your <strong>WordPress</strong> theme is <strong>' . $my_theme_name . ' version ' . $my_theme_version . '</strong>.</p>
 
-<p><span class="dashicons dashicons-awards"></span>Your website is <a href="' . $google_mobile_friendly_url . '">mobile friendly</a></p>
+  <p><span class="dashicons dashicons-awards"></span>Your website is <a href="' . $google_mobile_friendly_url . '">mobile friendly</a></p>
 
-<p><span class="dashicons dashicons-info"></span> <a target="_blank" href="' . $url . '/wp-content/themes/scaffold/readme.txt">Readme.txt</a> - theme information and change log</p>
+  <p><span class="dashicons dashicons-info"></span> <a target="_blank" href="' . $url . '/wp-content/themes/scaffold/readme.txt">Readme.txt</a> - theme information and change log</p>
 
-<p><span class="dashicons dashicons-awards"></span>This theme meets  <a href="' . $url . '/wp-admin/themes.php?page=themecheck">WordPress quality standards</a></p>
+  <p><span class="dashicons dashicons-awards"></span>This theme meets  <a href="' . $url . '/wp-admin/themes.php?page=themecheck">WordPress quality standards</a></p>
 
 
-<hr />
+  <hr />
 
-<p><img src="https://pbs.twimg.com/profile_images/446240204962013184/bPy0WSrL_400x400.jpeg" width="120" style="float:right; " />This theme is built and supported by <strong>Jason King</strong>, please contact him with any problems or questions.</p>
+  <p><img src="https://pbs.twimg.com/profile_images/446240204962013184/bPy0WSrL_400x400.jpeg" width="120" style="float:right; " />This theme is built and supported by <strong>Jason King</strong>, please contact him with any problems or questions.</p>
 
-<p><span class="dashicons dashicons-admin-links"></span> <a href="http://www.kingjason.co.uk">www.kingjason.co.uk</a></p>
+  <p><span class="dashicons dashicons-admin-links"></span> <a href="http://www.kingjason.co.uk">www.kingjason.co.uk</a></p>
 
-<p><span class="dashicons dashicons-email-alt"></span> <a href="mailto:jason@kingjason.co.uk">jason@kingjason.co.uk</a></p>
+  <p><span class="dashicons dashicons-email-alt"></span> <a href="mailto:jason@kingjason.co.uk">jason@kingjason.co.uk</a></p>
 
-<p><span class="dashicons dashicons-phone"></span> <strong>07414 755 856</strong>.</p>
+  <p><span class="dashicons dashicons-phone"></span> <strong>07414 755 856</strong>.</p>
 
-<p><span class="dashicons dashicons-twitter"></span> <a href="' . $twitter_url . '" target="_blank">@jasoncsking</a></p>
+  <p><span class="dashicons dashicons-twitter"></span> <a href="' . $twitter_url . '" target="_blank">@jasoncsking</a></p>
 
-<hr />
+  <hr />
 
-<p><span class="dashicons dashicons-welcome-learn-more"></span> Getting started? <a href="'. $rtfm .'" target="_blank">READ THE MANUAL</a> on Google Drive</p>
+  <p><span class="dashicons dashicons-welcome-learn-more"></span> Getting started? <a href="'. $rtfm .'" target="_blank">READ THE MANUAL</a> on Google Drive</p>
 
-<p><span class="dashicons dashicons-tickets"></span> Requests? Problems? <a href="mailto:jason@kingjason.co.uk">Email a support ticket</a> to <strong>jason@kingjason.co.uk</strong></p>';
+  <p><span class="dashicons dashicons-tickets"></span> Requests? Problems? <a href="mailto:jason@kingjason.co.uk">Email a support ticket</a> to <strong>jason@kingjason.co.uk</strong></p>';
 }
 
 /* 9. Add Demographic URL tool
