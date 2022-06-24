314798d5c024d222d220ee176c909531a7782f89
Phil T <362017+dtw@users.noreply.github.com>
2022-06-24 08:14:11 +0100

    Update panel branding

--- includes/widget-featured-page-hwbucks.php
+++ includes/widget-featured-page-hwbucks.php
@@ -49,9 +49,11 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 			$page_id = (int) $instance['page'];
 			$title = $instance['title'] ;
 			$show_excerpt = $instance['show_excerpt'];
-			$panel_colour = $instance['panel_colour'] ;
+			$bg_colour = $instance['bg_colour'] ;
+			$border_colour = $instance['border_colour'] ;
 			$show_btn = $instance['show_btn'];
 			$btn_text = $instance['btn_text'] ;
+			$btn_colour = $instance['btn_colour'] ;
 			$show_last_updated = $instance['show_last_updated'];
 			$last_updated_text = $instance['last_updated_text'];
 			$p = new WP_Query( array( 'page_id' => $page_id ) );
@@ -61,7 +63,7 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 				echo $before_widget;
 				?>
 
-				<?php echo '<div class="col-md-12 col-sm-12 col-xs-12 panel panel-' . $panel_colour . '">'?>
+				<?php echo '<div class="col-md-12 col-sm-12 col-xs-12 panel panel-' . $bg_colour . '">'?>
 					<div class="row">
 						<div class="col-md-8 col-sm-6 col-xs-12 panel-text">
 							<div class="row">
@@ -83,7 +85,7 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 									<?php	}
 									if ( $show_btn ) { ?>
 										<p class="clear-both">
-											<a class="btn btn-primary" href="<?php echo get_the_permalink(); ?>">
+											<?php echo '<a class="btn btn-primary btn-' . $btn_colour . '" href="' . get_the_permalink() . '">'; ?>
 												<?php echo $btn_text; ?>
 											</a>
 										</p>
@@ -94,7 +96,7 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 						<div class="col-md-4 col-sm-6 hidden-xs panel-icon">
 							<a class="img-anchor" href="
 								<?php the_permalink(); ?>" rel="bookmark">
-								<?php the_post_thumbnail('medium', array('class' => 'panel-icon-img')); ?>
+								<?php the_post_thumbnail('medium', array('class' => 'panel-icon-img border-colour-'.$border_colour)); ?>
 							</a>
 						</div>
 					</div>
@@ -113,9 +115,11 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 		$instance['page'] = (int)( $new_instance['page'] );
 		$instance['title'] = wp_strip_all_tags( $new_instance['title'] );
 		$instance['show_excerpt'] = isset( $new_instance['show_excerpt'] ) ? 1 : 0;
-		$instance['panel_colour'] = wp_strip_all_tags( $new_instance['panel_colour'] );
+		$instance['bg_colour'] = wp_strip_all_tags( $new_instance['bg_colour'] );
+		$instance['border_colour'] = wp_strip_all_tags( $new_instance['border_colour'] );
 		$instance['show_btn'] = isset( $new_instance['show_btn'] ) ? 1 : 0;
 		$instance['btn_text'] = wp_strip_all_tags( $new_instance['btn_text'] );
+		$instance['btn_colour'] = wp_strip_all_tags( $new_instance['btn_colour'] );
 		$instance['show_last_updated'] = isset( $new_instance['show_last_updated'] ) ? 1 : 0;
 		$instance['last_updated_text'] = wp_strip_all_tags( $new_instance['last_updated_text'] );
 		return $instance;
@@ -124,9 +128,11 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 		$page = isset( $instance['page'] ) ? (int) $instance['page'] : 0;
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : 'Hot news';
 		$show_excerpt = $instance['show_excerpt'];
-		$panel_colour = ! empty( $instance['panel_colour'] ) ? $instance['panel_colour'] : 'orange';
+		$bg_colour = ! empty( $instance['bg_colour'] ) ? $instance['bg_colour'] : 'light-blue';
+		$border_colour = ! empty( $instance['border_colour'] ) ? $instance['border_colour'] : 'none';
 		$show_btn = $instance['show_btn'];
 		$btn_text = ! empty( $instance['btn_text'] ) ? $instance['btn_text'] : 'Read more';
+		$btn_colour = ! empty( $instance['btn_colour'] ) ? $instance['btn_colour'] : 'blue';
 		$show_last_updated = $instance['show_last_updated'];
 		$last_updated_text = ! empty( $instance['last_updated_text'] ) ? $instance['last_updated_text'] : 'Last updated: ';
 		?>
@@ -139,15 +145,32 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'show_excerpt' ); ?>">Display excerpt?</label>
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('panel_colour'); ?>">Panel colour:
-				<select class='widefat' id="<?php echo $this->get_field_id('panel_colour'); ?>"
-						 name="<?php echo $this->get_field_name('panel_colour'); ?>" type="text">
+			<label for="<?php echo $this->get_field_id('bg_colour'); ?>">Background colour:
+				<select class='widefat' id="<?php echo $this->get_field_id('bg_colour'); ?>"
+						 name="<?php echo $this->get_field_name('bg_colour'); ?>" type="text">
 					<?php
 					/* This array and loop generates the rows for the dropdown menu. Blue results in panel-blue. Matching styles required in CSS */
-					$colourArray = ["Orange", "Blue", "Green", "Pink", "Turquoise","Coronavirus"];
+					$colourArray = ["Light-Blue", "Pink", "Gold", "Teal"];
 						foreach ($colourArray as $colour)  {
 							echo "<option value='" . strtolower($colour) . "'";
-							echo ($panel_colour==strtolower($colour))?'selected':'';
+							echo ($bg_colour==strtolower($colour))?'selected':'';
+							echo ">" . $colour . "</option>";
+						}
+					?>
+				</select>
+			</label>
+		</p>
+		<!-- Image border colour -->
+		<p>
+			<label for="<?php echo $this->get_field_id('border_colour'); ?>">Border colour:
+				<select class='widefat' id="<?php echo $this->get_field_id('border_colour'); ?>"
+						 name="<?php echo $this->get_field_name('border_colour'); ?>" type="text">
+					<?php
+					/* This array and loop generates the rows for the dropdown menu. Blue results in panel-blue. Matching styles required in CSS */
+						$colourArray = ["None", "Pink", "Blue", "Gold", "Teal"];
+						foreach ($colourArray as $colour)  {
+							echo "<option value='" . strtolower($colour) . "'";
+							echo ($border_colour==strtolower($colour))?'selected':'';
 							echo ">" . $colour . "</option>";
 						}
 					?>
@@ -161,6 +184,23 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 		<p>
 			<label for="<?php echo $this->get_field_id( 'btn_text' ); ?>">Button text:</label>
 			<input type="text" id="<?php echo $this->get_field_id( 'btn_text' ); ?>" name="<?php echo $this->get_field_name( 'btn_text' ); ?>" value="<?php echo esc_attr( $btn_text ); ?>" />
+		</p>
+		<!-- Button colour -->
+		<p>
+			<label for="<?php echo $this->get_field_id('btn_colour'); ?>">Button colour:
+				<select class='widefat' id="<?php echo $this->get_field_id('btn_colour'); ?>"
+						 name="<?php echo $this->get_field_name('btn_colour'); ?>" type="text">
+					<?php
+					/* This array and loop generates the rows for the dropdown menu. Blue results in panel-blue. Matching styles required in CSS */
+						$colourArray = ["Blue", "Light-Blue", "Gold", "Teal"];
+						foreach ($colourArray as $colour)  {
+							echo "<option value='" . strtolower($colour) . "'";
+							echo ($btn_colour==strtolower($colour))?'selected':'';
+							echo ">" . $colour . "</option>";
+						}
+					?>
+				</select>
+			</label>
 		</p>
 		<p>
 			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'show_last_updated' ); ?>" name="<?php echo $this->get_field_name( 'show_last_updated' ); ?>" <?php checked( $show_last_updated, 1 ); ?> />
