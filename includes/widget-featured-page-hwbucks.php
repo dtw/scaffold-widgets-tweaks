f9093243abbbf9db1b514cbd210b978977820c34
Phil T <362017+dtw@users.noreply.github.com>
2023-08-14 14:41:48 +0100

    Update var setting

--- includes/widget-featured-page-hwbucks.php
+++ includes/widget-featured-page-hwbucks.php
@@ -127,13 +127,13 @@ class SF_HWBucks_Featured_Page_Widget extends WP_Widget {
 	function form( $instance ) {
 		$page = isset( $instance['page'] ) ? (int) $instance['page'] : 0;
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : 'Hot news';
-		$show_excerpt = $instance['show_excerpt'];
+		$show_excerpt = ! empty( $instance['show_excerpt'] ) ? $instance['show_excerpt'] : '';
 		$bg_colour = ! empty( $instance['bg_colour'] ) ? $instance['bg_colour'] : 'light-blue';
 		$border_colour = ! empty( $instance['border_colour'] ) ? $instance['border_colour'] : 'none';
-		$show_btn = $instance['show_btn'];
+		$show_btn = ! empty( $instance['show_btn'] ) ? $instance['show_btn'] : '';
 		$btn_text = ! empty( $instance['btn_text'] ) ? $instance['btn_text'] : 'Read more';
 		$btn_colour = ! empty( $instance['btn_colour'] ) ? $instance['btn_colour'] : 'blue';
-		$show_last_updated = $instance['show_last_updated'];
+		$show_last_updated = ! empty( $instance['show_last_updated'] ) ? $instance['show_last_updated'] : '';
 		$last_updated_text = ! empty( $instance['last_updated_text'] ) ? $instance['last_updated_text'] : 'Last updated: ';
 		?>
 		<p>
